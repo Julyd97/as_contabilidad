8fe358c892ffc79729eab92f07057b947642c3d8
Julian <julyd97@gmail.com>
2021-12-09 23:11:04 -0500

    Finish[Proveedores]

--- proveedores/api.py
+++ proveedores/api.py
@@ -10,10 +10,18 @@ proveedor_schema = ProveedorSchema()
 api = Api(proveedores_app)
 
 class ProveedorListResource(Resource):
-    def get(self):
-        proveedores = Proveedor.get_all()
-        result = proveedor_schema.dump(proveedores, many=True)
-        return result
+    def get(self, num_cedula=None):
+        if num_cedula == None:
+            proveedores = Proveedor.get_all()
+            result = proveedor_schema.dump(proveedores, many=True)
+        else:  
+            proveedores = Proveedor.query.filter(Proveedor.numDocumento.like(str(num_cedula) + '%')).all()
+            if proveedores == []:
+                return  {'message': 'Ocurrio un error buscando con el numero dado'}, 404
+            else:         
+                result = proveedor_schema.dump(proveedores, many=True)
+        print(proveedores)
+        return result, 201
     def post(self):
         data = request.get_json()
         proveedor_dict = proveedor_schema.load(data)
@@ -33,14 +41,51 @@ class ProveedorListResource(Resource):
         proveedor.save()
         resp = proveedor_schema.dump(proveedor)
         return resp, 201
+    
+    
         
 class ProveedorResource(Resource):
     def get(self, proveedor_id):
         proveedor = Proveedor.get_by_id(proveedor_id)
         if proveedor is None:
-            raise ObjectNotFound('El proveedor no existe')
+            raise ('El proveedor no existe')
         resp = proveedor_schema.dump(proveedor)
         return resp
+    def put(self, proveedor_id):
+        proveedor = Proveedor.get_by_id(proveedor_id)
+        if proveedor is None:
+            raise ('El proveedor no existe')
+        data = request.get_json()
+        try:
+            proveedor_dict = proveedor_schema.load(data)
+            proveedor.tipoDocumento = proveedor_dict['tipoDocumento']
+            proveedor.proveedornumDocumento = proveedor_dict['numDocumento']
+            proveedor.pais = proveedor_dict['pais']
+            proveedor.departamento = proveedor_dict['departamento']
+            proveedor.municipio = proveedor_dict['municipio']
+            proveedor.primerNombre = proveedor_dict['primerNombre']
+            proveedor.segundoNombre = proveedor_dict['segundoNombre']
+            proveedor.primerApellido = proveedor_dict['primerApellido']
+            proveedor.segundoApellido = proveedor_dict['segundoApellido']
+            proveedor.direccion = proveedor_dict['direccion']
+            proveedor.telefono = proveedor_dict['telefono']
+            proveedor.correo = proveedor_dict['correo']
+            proveedor.codigoPostal = proveedor_dict['codigoPostal']
+            
+            proveedor.save()
+            resp = proveedor_schema.dump(proveedor)
+            return resp, 201
+        except:
+            return {'message': 'Ocurrio un error modificando el proveedor'}, 400
+    def  delete(self, proveedor_id):
+        proveedor = Proveedor.get_by_id(proveedor_id)
+        if proveedor is None:
+            raise ('El proveedor no existe')
+        try:
+            proveedor.delete()
+            return 201
+        except:
+            return {'message': 'Un error ocurrio un error eliminando el proveedor'}, 400
 
-api.add_resource(ProveedorListResource, '/api/v1.0/proveedores/', endpoint='proveedores_list_resource')
+api.add_resource(ProveedorListResource, '/api/v1.0/proveedores/', '/api/v1.0/proveedores/<num_cedula>', endpoint='proveedores_list_resource')
 api.add_resource(ProveedorResource, '/api/v1.0/proveedor/<int:proveedor_id>', endpoint='proveedor_resource')
--- static/js/script.js
+++ static/js/script.js
@@ -61,7 +61,6 @@ DOMModal.addEventListener('show.bs.modal', function (event) {
   modalTercero.checked = tercero
   modalProveedor.checked = proveedor
   modalCosto.checked = costo
-  console.log(cartera)
   //modalBodyInput.value = recipient
 })
 // Click boton de modificar
@@ -107,15 +106,24 @@ buttonclose.addEventListener('click', function(){
 })
 let buttonbuscar = document.getElementById('buttonbuscar')
 buttonbuscar.addEventListener('click', function(){
-  let buscar = document.getElementById('textbuscar')
-  if(buscar.style.display !== 'none'){
-    buscar.style.display = 'none'
-  }else{
-    buscar.style.display = "inline"
-  }
-  
+  buscarproveedor();
 })
 
+document.querySelector('#textbuscar').addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    buscarproveedor();
+  }
+}); 
+
+function buscarproveedor (){
+  let buscar = document.getElementById('textbuscar')
+  if(buscar.innerHTML != null){
+    let url = '/api/v1.0/proveedores/'
+    loadtable(url + buscar.value)
+  }else{
+    
+  }
+}
 // api url
 const api_url =
 	"/api/v1.0/proveedores/";
@@ -131,7 +139,7 @@ function sendapi(url,data){
   }).then(response => response.json())
   .then(data => {
     alert("Proveedor creado");
-    
+    loadtable(url)
   })
   
 
@@ -142,16 +150,98 @@ async function getapi(url) {
 	// Storing response
 	const response = await fetch(url);
 	
+  console.log(response.ok)
+  console.log(response.status)
 	// Storing data in form of JSON
-	var data = await response.json();
-	console.log(data);
-	// if (response) {
-	// 	hideloader();
-	// }
-	show(data);
+  if (response.ok == true){ 
+    var data = await response.json();
+    console.log(data);
+    console.log('GHola')
+    show(data);
+  } else{
+    alert('El proveedor que esta buscando no se encuentra, revise la busqueda')
+  }
+
 }
 // Calling that async function
-getapi(api_url);
+
+//getapi(api_url);
+
+//Load data of table proveedores
+async function loadtable(url){
+  await getapi(url);
+  const tbodys = document.querySelectorAll('#proveedores tr');
+
+  let DOMModals = document.getElementById('modalModifyProveedor');
+  console.log(tbodys);
+  let modalElements = new bootstrap.Modal(DOMModals, {
+    keyboard: false
+  });
+  
+  for (var i = 1; i < tbodys.length; i++){
+    tbodys[i].addEventListener('click', function() {
+      modalElements.show(this);
+    });
+  }
+  DOMModals.addEventListener('show.bs.modal', function (event) {
+    // Button that triggered the modal
+    var button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    
+    let primerNombre = button.childNodes[1].innerHTML
+    let segundoNombre = button.childNodes[3].innerHTML
+    let primerApellido = button.childNodes[5].innerHTML
+    let segundoApellido = button.childNodes[7].innerHTML
+    let tipoDocumento = button.childNodes[9].innerHTML
+    let numDocumento = button.childNodes[11].innerHTML
+    let correo = button.childNodes[13].innerHTML
+    let pais = button.childNodes[15].innerHTML
+    let departamento = button.childNodes[17].innerHTML
+    let municipio = button.childNodes[19].innerHTML
+    let direccion = button.childNodes[21].innerHTML
+    let codigoPostal = button.childNodes[23].innerHTML
+    let telefono = button.childNodes[25].innerHTML
+    let id = button.childNodes[27].innerHTML
+    // If necessary, you could initiate an AJAX request here
+    // and then do the updating in a callback.
+    //
+    // Update the modal's content.
+    let modalPrimerNombre = DOMModals.querySelector('#primerNombrem')
+    let modalSegundoNombre = DOMModals.querySelector('#segundoNombrem')
+    let modalPrimerApellido = DOMModals.querySelector('#primerApellidom')
+    let modalSegundoApellido = DOMModals.querySelector('#segundoApellidom')
+    let modalTipoDocumento = DOMModals.querySelector('#tipoDocumentom')
+    let modalNumeroDocumento = DOMModals.querySelector('#numDocumentom')
+    let modalCorreo = DOMModals.querySelector('#correom')
+    let modalPais = DOMModals.querySelector('#paism')
+    let modalDepartamento = DOMModals.querySelector('#departamentom')
+    let modalMunicipio = DOMModals.querySelector('#municipiom')
+    let modalDireccion = DOMModals.querySelector('#direccionm')
+    let modalCodigoPostal = DOMModals.querySelector('#codigoPostalm')
+    let modalTelefono = DOMModals.querySelector('#telefonom')
+    let modalId = DOMModals.querySelector("#idm")
+  
+    //var modalBodyInput = DOMModal.querySelector('.modal-body input')
+    modalPrimerNombre.value = primerNombre
+    modalSegundoNombre.value = segundoNombre
+    modalPrimerApellido.value = primerApellido
+    modalSegundoApellido.value = segundoApellido
+    modalTipoDocumento.value = tipoDocumento
+    modalNumeroDocumento.value = numDocumento
+    modalCorreo.value = correo
+    modalPais.value = pais
+    modalDepartamento.value = departamento
+    modalMunicipio.value = municipio
+    modalDireccion.value = direccion
+    modalCodigoPostal.value = codigoPostal
+    modalTelefono.value = telefono
+    modalId.value = id
+    //modalBodyInput.value = recipient
+  })
+  
+}
+console.log('hola')
+loadtable(api_url);
 
 // Function to hide the loader
 // function hideloader() {
@@ -187,41 +277,163 @@ buttonEnviar.addEventListener('click',function(){
 function show(data) {
 	let tab =
 		`<tr>
-		<th>Pais</th>
-		<th>Codigo Postal</th>
-		<th>Primer Nombre</th>
+    <th>Primer Nombre</th>
 		<th>Segundo Nombre</th>
 		<th>Primer Apellido</th>
     <th>Segundo Apellido</th>
-    <th>Departamento</th>
-    <th>Telefono</th>
-    <th>Correo</th>
-    <th>Municipio</th>
     <th>Tipo de Documento</th>
     <th>Numero de Documento</th>
+    <th>Correo</th>
+		<th>Pais</th>
+    <th>Departamento</th>
+    <th>Municipio</th>
     <th>Dirección</th>
+		<th>Codigo Postal</th>    
+    <th>Telefono</th>
+    <th class="d-none">id</th>  
     </tr>`;
-	
-	// Loop to access all rows
-	for (let r of data) {
-		tab += `<tr>
-	<td>${r.pais} </td>
-	<td>${r.codigoPostal}</td>
-	<td>${r.primerNombre}</td>
-	<td>${r.segundoNombre}</td>
-  <td>${r.primerApellido}</td>
-  <td>${r.segundoApellido}</td>
-  <td>${r.departamento}</td>
-  <td>${r.telefono}</td>
-  <td>${r.correo}</td>
-  <td>${r.municipio}</td>
-  <td>${r.tipoDocumento}</td>
-  <td>${r.numDocumento}</td>
-  <td>${r.direccion}</td>		
-</tr>`;
-	}
+
+    for (let r of data) {
+      tab += `<tr>
+    <td data-pn=${r.primerNombre} data-sn=${r.segundoNombre} data-pa=${r.primerApellido} data-sa=${r.segundoApellido}>${r.primerNombre}</td>
+    <td>${r.segundoNombre}</td>
+    <td>${r.primerApellido}</td>
+    <td>${r.segundoApellido}</td>
+    <td>${r.tipoDocumento}</td>
+    <td>${r.numDocumento}</td>
+    <td>${r.correo}</td>
+    <td>${r.pais} </td>
+    <td>${r.departamento}</td>
+    <td>${r.municipio}</td>
+    <td>${r.direccion}</td>	
+    <td>${r.codigoPostal}</td>
+    <td>${r.telefono}</td>
+    <td class="d-none">${r.id}</td>
+  </tr>`;
+  }
 	// Setting innerHTML as tab variable
 	document.getElementById("proveedores").innerHTML = tab;
 }
+// Choose fields from proveedores table
 
+// Block fields from modal #modalnewpeoveedor
+let ModalModifyProveedor = document.getElementById('modalModifyProveedor')
+ModalModifyProveedor.addEventListener('hidden.bs.modal', function () {
+    let primerNombre = document.getElementById('primerNombrem') 
+    let segundoNombre = document.getElementById('segundoNombrem')
+    let primerApellido = document.getElementById('primerApellidom')
+    let segundoApellido = document.getElementById('segundoApellidom')
+    let tipoDocumento = document.getElementById('tipoDocumentom')
+    let numDocumento = document.getElementById('numDocumentom')
+    let correo = document.getElementById('correom')
+    let pais = document.getElementById('paism')
+    let departamento = document.getElementById('departamentom')
+    let municipio = document.getElementById('municipiom')
+    let direccion = document.getElementById('direccionm')
+    let codigoPostal = document.getElementById('codigoPostalm')
+    let telefono = document.getElementById('telefonom')
+    primerNombre.disabled = true
+    segundoNombre.disabled = true
+    primerApellido.disabled = true
+    segundoApellido.disabled = true
+    tipoDocumento.disabled = true
+    numDocumento.disabled = true
+    correo.disabled = true
+    pais.disabled = true
+    departamento.disabled = true
+    municipio.disabled = true
+    direccion.disabled = true
+    codigoPostal.disabled = true
+    telefono.disabled = true
+})
+// Modificar button de proveedores
+let buttonModificarProveedor = document.getElementById('modificarProveedor')
+buttonModificarProveedor.addEventListener('click', function(){
+  let primerNombre = document.getElementById('primerNombrem') 
+  let segundoNombre = document.getElementById('segundoNombrem')
+  let primerApellido = document.getElementById('primerApellidom')
+  let segundoApellido = document.getElementById('segundoApellidom')
+  let tipoDocumento = document.getElementById('tipoDocumentom')
+  let numDocumento = document.getElementById('numDocumentom')
+  let correo = document.getElementById('correom')
+  let pais = document.getElementById('paism')
+  let departamento = document.getElementById('departamentom')
+  let municipio = document.getElementById('municipiom')
+  let direccion = document.getElementById('direccionm')
+  let codigoPostal = document.getElementById('codigoPostalm')
+  let telefono = document.getElementById('telefonom')
+  primerNombre.disabled = false
+  segundoNombre.disabled = false
+  primerApellido.disabled = false
+  segundoApellido.disabled = false
+  tipoDocumento.disabled = false
+  numDocumento.disabled = false
+  correo.disabled = false
+  pais.disabled = false
+  departamento.disabled = false
+  municipio.disabled = false
+  direccion.disabled = false
+  codigoPostal.disabled = false
+  telefono.disabled = false
+})
+// Send to modify in the api
+function modifyinapi(url,data){
+  
+  fetch(url, {
+    method: 'PUT', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  }).then(response => response.json())
+  .then(data => {
+    alert("Proveedor modificado");
+    loadtable(api_url)
+  })
+}
+
+function deleteapi(url){
+  fetch(url, {
+    method: 'DELETE', // or 'PUT'
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  }).then(response => response.json())
+  .then(data => {
+    alert("Proveedor eliminado");
+    loadtable(api_url)
+  })
+}
+
+let buttonGuardarProveedor = document.getElementById('guardarProveedor')
+buttonGuardarProveedor.addEventListener('click',function(){
+  let primerNombre1 = document.getElementById('primerNombrem').value
+  let segundoNombre1 = document.getElementById('segundoNombrem').value
+  let primerApellido1 = document.getElementById('primerApellidom').value
+  let segundoApellido1 = document.getElementById('segundoApellidom').value
+  let tipoDocumento1 = document.getElementById('tipoDocumentom').value
+  let numDocumento1 = document.getElementById('numDocumentom').value
+  let correo1 = document.getElementById('correom').value
+  let pais1 = document.getElementById('paism').value
+  let departamento1 = document.getElementById('departamentom').value
+  let municipio1 = document.getElementById('municipiom').value
+  let direccion1 = document.getElementById('direccionm').value
+  let codigoPostal1 = document.getElementById('codigoPostalm').value
+  let telefono1 = document.getElementById('telefonom').value
+  let id1 = document.getElementById('idm').value
+
+  let data = {primerNombre: primerNombre1, segundoNombre: segundoNombre1, primerApellido: primerApellido1, 
+              segundoApellido: segundoApellido1, tipoDocumento: tipoDocumento1, numDocumento: numDocumento1,
+              correo: correo1, pais:pais1, departamento: departamento1, municipio:municipio1, direccion:direccion1, codigoPostal:codigoPostal1, telefono:telefono1 }
+    let url = '/api/v1.0/proveedor/'.concat(id1)
+    modifyinapi(url,data);
+});
+
+
+let buttonBorrarProveedor = document.getElementById('borrarProveedor')
+buttonBorrarProveedor.addEventListener('click',function(){
+  let id1 = document.getElementById('idm').value
+    let url = '/api/v1.0/proveedor/'.concat(id1)
+    deleteapi(url);
+});
 
